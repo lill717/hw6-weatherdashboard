85e7eeaca437bac9717c0cb48f710bf6255adecc
Lillian Chan <lilchan717@gmail.com>
2021-06-23 22:12:43 -0500

    Html and css mostly done. Started on js

---
+++ Assets/js/javascipt.js
@@ -0,0 +1,39 @@
+//clear form lesson 5-5 js
+
+var currentDayEl = moment();
+$('.lead').text(currentDayEl.format("dddd, MMMM Do"));
+console.log(currentDayEl);
+
+var currentHourEl = moment();
+$(".currentHour").text(currentHourEl.format("hh:" + "mm" + "a"));
+console.log(currentHourEl);
+
+
+function date() {
+    
+}
+
+
+function searchCity() {
+
+}
+
+
+
+function saveCity() {
+
+}
+
+
+
+
+function displayCity() {
+
+}
+
+
+
+
+function fiveDay() {
+    
+}
