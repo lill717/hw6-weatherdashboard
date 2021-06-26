f516afe872dc470f5a76de53d5424d0cf4db60e1
Lillian Chan <lilchan717@gmail.com>
2021-06-26 12:36:50 -0500

    City search, daily weather, and UV index working and rendering.

--- Assets/js/js2.js
+++ Assets/js/js2.js
@@ -3,39 +3,25 @@ var city = "";
 
 const iconUrl = "https://api.openweathermap.org/img/wn/";
 const apiKey = "f3e06e3f6da3749bb7e73025459954e1";
-const tempUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
 let fiveDayUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 let weatherCard = $(".card");
 let forcastCard = $(".fiveday");
 
-
-// Search for city
-// function cityWeatherSearch() {
-//     fetch(tempUrl)
-//         if (city == "") {
-//             city == cityInput;
-//             citySearch(tempUrl);
-//         }
-//         else {
-//             city == cityInput;
-//             today.subtract(5, "days");
-//             currentWeather(tempUrl);
-//         }
-// }
-
-
-
 // Get current forecast to display along with temperature, the humidity, the wind speed, and the UV index
-function currentWeather(url) {
-    fetch(url)
+function currentWeather(city) {
+
+const tempUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+console.log(tempUrl)
+    fetch(tempUrl)
         .then(function (response) {
             return response.json();
-            console.log(doooog);
+            // console.log("dog");
         })
         .then(function (data) {
-            console.log(doooog);
+            // console.log("doooog");
 
+            console.log(data    )
             let cityAndDate = $("#saved-city-name");
             let iconWeather = $("temp");
             let temp = $("#temp");
@@ -57,102 +43,97 @@ function currentWeather(url) {
             humidity.text("Humidity: " + data.main.humidity + "%");
             windSpeed.text("Wind: " + data.wind.speed + "mph");
 
-            cityAndDate.append(iconWeather);
-            weatherCard.append(cityAndDate);
-            weatherCard.append(temp);
-            weatherCard.append(humidity);
-            weatherCard.append(windSpeed);
-
             let lat = data.coord.lat;
             let lon = data.coord.lon;
-            currentWeather(lat, lon, fiveDayUrl);
+            currentWeather2(lat, lon, fiveDayUrl);
         });
 }
 
-function currentWeather(lat, lon, tempUrl) {
-    // tempUrl = tempUrl + lat + "=&lat" + lon + "=&lon" + "&exclude=minutely,hourly&alerts" + "&appid=" + apiKey;
+function currentWeather2(lat, lon, tempUrl) {
+    tempUrl = fiveDayUrl + lat + "&lon="+ lon  + "&exclude=minutely,hourly&alerts" + "&appid=" + apiKey;
     fetch(tempUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            console.log(data)
             let uvIndex = $("#uv-index");
-            uvIndex.text = (data.current.uvIndex);
-            uvIndex.append();       // Do I need anything in () ???
+            uvIndex.text(data.current.uvi);
 
-            if (data.current.uvIndex < 3) {
-                uvText.attr("class", "uvIndex-low");
+            if (data.current.uvi < 3) {
+                uvIndex.attr("class", "uvIndex-low");
+                uvIndex.css("background-color", "blue");
             }
-            else if (data.current.uvIndex < 6) {
-                uvText.attr("class", "uvIndex-medium");
+            else if (data.current.uvi < 6) {
+                uvIndex.attr("class", "uvIndex-medium");
+                uvIndex.css("background-color", "#56bc56");
+
             }
-            else if (data.current.uvIndex < 8) {
-                uvText.attr("class", "uvIndex-high");
+            else if (data.current.uvi < 8) {
+                uvIndex.attr("class", "uvIndex-high");
+                uvIndex.css("background-color", "yellow");
+
             }
-            else if (data.current.uvIndex < 11) {
-                uvText.attr("class", "uvIndex-very-high");
+            else if (data.current.uvi < 11) {
+                uvIndex.attr("class", "uvIndex-very-high");
+                uvIndex.css("background-color", "orange");
+
             }
-            else if (data.current.uvIndex > 10) {
-                uvText.attr("class", "uvIndex-extremely-high");
+            else if (data.current.uvi > 10) {
+                uvIndex.attr("class", "uvIndex-extremely-high");
+                uvIndex.css("background-color", "red");
+
             }
 
             console.log(data);
 
+            for(var i = 0; i < 5; i++){
+                console.log(data.daily[i]);
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
         })
+    }
+ 
+    // }
 
-    function citySearch(cityName) {
-        if (city == "") {
-            city == cityName;
-            currentWeather(tempUrl);
-        }
-        else {
-            city = cityName;
-            today.subtract(5, "days");
-            upWeather(tempUrl);
-        }
-    }
-
-    function savedCities(city) {
-        if (citySearch.length === SEARCH_LIMIT) {
-            citySearch.pop();
-        }
-        citySearch.push(cityArrayToStore);
-        localStorage.setItem(KEY_CITY, JSON.stringify(citySearch));
-    }
-    // Retrieves locally stored cities.
-    function retrieveCities() {
-        if (localStorage.getItem(city)) {
-            return JSON.parse(localStorage.getItem(city));
-        } else {
-            return citySearch;
-        }
-    }
+    // function savedCities(city) {
+    //     if (citySearch.length === SEARCH_LIMIT) {
+    //         citySearch.pop();
+    //     }
+    //     citySearch.push(cityArrayToStore);
+    //     localStorage.setItem(KEY_CITY, JSON.stringify(citySearch));
+    // }
+    // // Retrieves locally stored cities.
+    // function retrieveCities() {
+    //     if (localStorage.getItem(city)) {
+    //         return JSON.parse(localStorage.getItem(city));
+    //     } else {
+    //         return citySearch;
+    //     }
+    // }
     // citiesSearched = retrieveCities();
 
 
 
 
-    // function upWeather(fiveDayUrl) {
-    //     fetch(fiveDayUrl)
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (data) {
-    //             console.log(data);
-}
-
-
-    // var searchValue = $("#search").val()
-    // searchCity(searchValue);
-    // searchForecast();
-    // searchUVIndex();
 
 $("#search").on("click", function (e) {
     e.preventDefault();
-    console.log(data);
+    console.log("search btn clicked");
 
     var value = $("#city-name").val();
-    `fetch(apiUrl).then(function (response) {
-    console.log(value);
-} ) ;
-} ) 
+    currentWeather(value);
+
+})
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # hw6-weatherdashboard
+
+
+# Project name
+This is a weather dashboard with a 5 day forecast.
+
+ <!-- I work a busy part time job that requires a lot of scheduled appointments. To be time efficient, I needed a daily planner to keep track of all of my important events. I need it colorcoded to tell me which events have passed and which have yet to occur that day. -->
+
+
+Screenshot of the weather dashboard:
+
+
+
+
+
+
+
+
+# Prerequisites
+Before you begin, ensure you have met the following requirements:
+* You have installed the latest version of Visual Studio Code. <img width="40" alt="Visual Studio Code logo" src="https://user-images.githubusercontent.com/84090538/120902068-d96e0600-c603-11eb-9b59-949a731514b4.png" />
+* You are using a Windows or Mac OS machine. OS version 10 or above.
+* You have installed the lastest Google Chrome browser.
+
+# Installing Portfolio application
+To install my portfolio application, follow these steps:
+macOS and Windows:
+1. Open a new tab in Chrome
+2. Then press Ctrl (Windows) or Cmd (Mac) + O. This will bring up the Open File menu. 
+4. Find your HTML file and open it with Visual Studion Code.
+5. On the left panel, find *index.html* file and right click on it.
+6. When drop down menu appears, select *Open in Other Browser" to choose Chrome.
+7. This application should appear in the Chrome browser for your perusal.
+<img width="204" alt="Directions to open" src="https://user-images.githubusercontent.com/84090538/120901902-0a017000-c603-11eb-9241-723a6de5f306.png" />
+
+
+# Website Link
+<!-- https://github.com/lill717/hw6.git -->
